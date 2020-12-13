2d464601fca8a85b616402bbf3a759109a375858
sanjib <sanjib12sinha@gmail.com>
2020-12-13 06:04:24 +0530

    Second Commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:inherited_widget_explained/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(InheritedWidgetApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
